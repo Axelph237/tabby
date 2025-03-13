9b3aa3396e5b9ed285590a02b9f1601e7935f0a8
CandyCaneCazoo <aiden.j.king004@gmail.com>
2025-03-13 11:00:11 -0700

    testing prettier

--- app/menu/menu.tsx
+++ app/menu/menu.tsx
@@ -29,14 +29,14 @@ export default function Menu() {
           {/*<div className="relative bg-accent w-1/2 h-[85px] rounded-tl-2xl rounded-tr-2xl">*/}
           {/*    <div className="absolute bottom-0 right-0 tl-corner w-[30px] h-[30px] bg-primary"></div>*/}
           {/*</div>*/}
-          <div className="bg-primary tl-corner h-[16px] w-[16px]"></div>
-          <div className="bg-primary relative right-0 h-[85px] w-1/2 rounded-tl-2xl rounded-tr-2xl">
-            <h1 className="font-dongle text-accent flex items-center justify-center text-[64px] font-semibold">
+          <div className="tl-corner h-[16px] w-[16px] bg-primary"></div>
+          <div className="relative right-0 h-[85px] w-1/2 rounded-tl-2xl rounded-tr-2xl bg-primary">
+            <h1 className="flex items-center justify-center font-dongle text-[64px] font-semibold text-accent">
               Menu
             </h1>
           </div>
         </div>
-        <div className="bg-primary flex flex-col items-center gap-10 rounded-tl-2xl p-[20px] sm:p-[30px] md:p-[50px] lg:p-[60px]">
+        <div className="flex flex-col items-center gap-10 rounded-tl-2xl bg-primary p-[20px] sm:p-[30px] md:p-[50px] lg:p-[60px]">
           <MenuItem item={testItems[0]} />
           <MenuItem item={testItems[1]} />
           <MenuItem item={testItems[0]} />
@@ -55,14 +55,14 @@ function MenuItem({ item }: { item: Item }) {
       {/* shape */}
       <div className="absolute flex w-full flex-row">
         {/* shape - body */}
-        <div className="bg-secondary h-[200px] w-2/3 rounded-tl-2xl rounded-br-2xl rounded-bl-2xl"></div>
+        <div className="h-[200px] w-2/3 rounded-tl-2xl rounded-br-2xl rounded-bl-2xl bg-secondary"></div>
         {/* shape - info */}
         <div className="flex w-1/3 flex-col">
-          <div className="bg-secondary h-[150px] w-full rounded-tr-2xl rounded-br-2xl"></div>
-          <div className="bg-secondary br-corner h-[16px] w-[16px]"></div>
+          <div className="h-[150px] w-full rounded-tr-2xl rounded-br-2xl bg-secondary"></div>
+          <div className="br-corner h-[16px] w-[16px] bg-secondary"></div>
         </div>
       </div>
-             {/* Added comment */}
+      {/* Added comment */}
       {/* Sections */}
       <div className="relative flex h-[200px] w-full flex-row gap-[10px] p-[10px]">
         {/* Body */}
@@ -80,7 +80,7 @@ function MenuItem({ item }: { item: Item }) {
 
         {/* Buttons */}
         <div className="absolute right-0 bottom-0 flex h-[50px] w-1/3 items-center justify-center p-[5px]">
-          <div className="bg-accent flex size-full cursor-pointer items-center justify-center rounded-xl font-bold">
+          <div className="flex size-full cursor-pointer items-center justify-center rounded-xl bg-accent font-bold">
             Add
           </div>
         </div>
