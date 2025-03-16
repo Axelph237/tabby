9923dec4bd1b66c2b33e6e249db20ff38471033e
CandyCaneCazoo <aiden.j.king004@gmail.com>
2025-03-16 00:03:34 -0700

    refactoring

--- app/root.tsx
+++ app/root.tsx
@@ -9,7 +9,7 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
-import Filters from "~/components/filters";
+import Svgfilters from "~/components/svgfilters";
 
 export const links: Route.LinksFunction = () => [
 	{ rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -42,7 +42,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 				<Links />
 			</head>
 			<body>
-				<Filters />
+				<Svgfilters />
 				{children}
 				<ScrollRestoration />
 				<Scripts />
