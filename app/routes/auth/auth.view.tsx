f3105101b72be459ccb4e1b90c28face61acc798
CandyCaneCazoo <aiden.j.king004@gmail.com>
2025-04-09 13:57:53 -0700

    Updated login page

--- app/routes/auth/auth.view.tsx
+++ app/routes/auth/auth.view.tsx
@@ -1,5 +1,37 @@
+import "/public/styles/menu.css";
 import { Outlet } from "react-router";
+import { TabbyLogo } from "~/components/icons";
 
-export default function LoginPage() {
-	return <Outlet />;
+export default function AuthPage() {
+	return (
+		<main
+			id="order-page-main"
+			className="relative flex h-screen w-screen flex-col justify-end overflow-hidden px-[20px]"
+		>
+			<div
+				id="checkout-header"
+				className="top-0 z-10 flex w-full flex-row items-center justify-between text-primary"
+			>
+				<h1 className="font-dongle text-[64px]">Tabby</h1>
+				<TabbyLogo className="h-26 w-26" />
+			</div>
+			{/* Menu */}
+			<div
+				id="menu-container"
+				className="gooey relative top-[30px] flex h-full flex-col"
+			>
+				<div className="flex flex-row items-end justify-end">
+					<div className="relative right-0 w-1/2 bg-primary">
+						<h1 className="flex items-center justify-center font-dongle text-[64px] font-semibold text-accent">
+							Login
+						</h1>
+					</div>
+				</div>
+				<div className="z-50 flex h-full flex-col items-center justify-start bg-primary p-[20px] shadow-lg sm:p-[30px] md:p-[50px] lg:p-[60px]">
+					{/*	Input */}
+					<Outlet />
+				</div>
+			</div>
+		</main>
+	);
 }
--- app/routes/auth/login.view.tsx
+++ app/routes/auth/login.view.tsx
@@ -1,14 +1,113 @@
-const loginUrl = "http://localhost:3000/auth/google";
+import { Fragment, type ReactNode } from "react";
+import { GoogleLogoIcon, LockIcon, UserIcon } from "~/components/icons";
+import "./login.css";
+
+const providers = [
+	{
+		label: "Google",
+		icon: <GoogleLogoIcon className="icon-sm" />,
+		authLink: "http://localhost:3000/auth/google",
+	},
+];
 
 export default function LoginLoginPage() {
 	return (
-		<div>
-			<a
-				className="btn"
-				href={loginUrl}
-			>
-				Login
-			</a>
+		<div className="flex flex-col gap-4">
+			<UserInput
+				id="username"
+				icon={<UserIcon className="icon-md" />}
+				label={"Username"}
+				placeholder={"Your user name"}
+				className="font-dongle"
+			/>
+			<UserInput
+				id="password"
+				icon={<LockIcon className="icon-md" />}
+				label={"Password"}
+				placeholder={"password"}
+				className="font-redacted-script"
+				type="password"
+			/>
+			{/* Dividers */}
+			<div className="flex flex-row items-center justify-center gap-2">
+				<svg
+					width="100%"
+					height="4"
+				>
+					<line
+						x1="0"
+						y1="50%"
+						x2="100%"
+						y2="50%"
+						stroke="var(--color-accent)"
+						strokeWidth="2"
+					/>
+				</svg>
+				<p className="text-lg font-bold whitespace-nowrap text-accent">
+					Or with
+				</p>
+				<svg
+					width="100%"
+					height="4"
+				>
+					<line
+						x1="0"
+						y1="50%"
+						x2="100%"
+						y2="50%"
+						stroke="var(--color-accent)"
+						strokeWidth="2"
+					/>
+				</svg>
+			</div>
+
+			{/* Providers */}
+			<ul className="flex flex-row items-center justify-center text-primary">
+				{providers.map((provider, i) => (
+					<Fragment key={i}>
+						<a
+							href={provider.authLink}
+							className="w-fit cursor-pointer rounded-full bg-accent p-3 shadow-xl transition-all duration-200 hover:bg-secondary"
+						>
+							{provider.icon}
+						</a>
+					</Fragment>
+				))}
+			</ul>
+		</div>
+	);
+}
+
+interface UserInputProps {
+	icon: ReactNode;
+	label: string;
+	placeholder: string;
+	className?: string;
+	type?: string;
+	id: string;
+}
+
+function UserInput(props: UserInputProps) {
+	return (
+		<div
+			id={props.id}
+			className={`${props.className} user-input`}
+		>
+			<div>{props.icon}</div>
+			<div className="flex w-full flex-col items-start justify-start">
+				<label
+					htmlFor={props.id + "-input"}
+					className="text-md font-red-hat-display font-bold"
+				>
+					{props.label}
+				</label>
+				<input
+					id={props.id + "-input"}
+					className="w-full text-3xl outline-none"
+					type={props.type ?? "text"}
+					placeholder={props.placeholder}
+				/>
+			</div>
 		</div>
 	);
 }
