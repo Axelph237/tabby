7f5b636801742892c89e1769b5335dc6b516b1d7
CandyCaneCazoo <aiden.j.king004@gmail.com>
2025-04-16 10:11:57 -0700

    Extended session details to have item selections

--- app/routes/menu/menu.validation.ts
+++ app/routes/menu/menu.validation.ts
@@ -2,32 +2,64 @@ import { Type as t, type Static } from "@sinclair/typebox";
 import { uuidObj } from "~/utils/types/uuid";
 import { Nullable } from "~/utils/types/nullable";
 
-export const menuObj = t.Object({
+// DEPRECATED
+export const menuTObj = t.Object({
 	id: uuidObj,
 	created_at: t.Date(),
 	created_by: uuidObj,
 	name: t.String(),
 });
-export type Menu = Static<typeof menuObj>;
+export type Menu = Static<typeof menuTObj>;
 
-export const itemOnMenuObj = t.Object({
+// DEPRECATED
+export const itemOnMenuTObj = t.Object({
 	item_id: t.Integer(),
 	menu_id: uuidObj,
 });
-export type ItemOnMenu = Static<typeof itemOnMenuObj>;
+export type ItemOnMenu = Static<typeof itemOnMenuTObj>;
 
-export const itemObj = t.Object({
+export const itemOptionTObj = t.Object({
+	label: t.String(),
+	type: t.Union([t.Literal("one"), t.Literal("many"), t.Literal("text")]),
+});
+export type ItemOption = Static<typeof itemOptionTObj>;
+
+export const itemSelectTObj = t.Object({
+	label: t.String(),
+	price: t.Integer(),
+	is_default: t.Boolean(),
+});
+export type ItemSelection = Static<typeof itemSelectTObj>;
+
+export const itemTObj = t.Object({
 	id: t.Integer(),
 	name: t.String(),
 	description: t.Optional(Nullable(t.String())),
 	img_url: t.Optional(Nullable(t.String({ format: "uri" }))),
 	base_price: t.Integer(),
 });
-export type Item = Static<typeof itemObj>;
+export type Item = Static<typeof itemTObj>;
 
-export const sessionDetailsObj = t.Object({
+export const sessionDetailsTObj = t.Object({
 	menu_name: t.String(),
 	expires_at: t.Date(),
-	items: t.Array(itemObj),
+	items: t.Array(
+		t.Intersect([
+			// Item
+			itemTObj,
+			t.Object({
+				// Options
+				options: t.Array(
+					t.Intersect([
+						itemOptionTObj,
+						t.Object({
+							// Selections
+							selections: t.Array(itemSelectTObj),
+						}),
+					]),
+				),
+			}),
+		]),
+	),
 });
-export type SessionDetails = Static<typeof sessionDetailsObj>;
+export type SessionDetails = Static<typeof sessionDetailsTObj>;
