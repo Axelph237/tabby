f810dc65b9d1af856d0427944163396bae2f8369
CandyCaneCazoo <aiden.j.king004@gmail.com>
2025-05-23 23:01:06 -0700

    Added arrow icon

--- app/components/icons.tsx
+++ app/components/icons.tsx
@@ -243,3 +243,24 @@ export function PenIcon(props: SVGProps<SVGSVGElement>) {
 		</svg>
 	);
 }
+
+export function RightArrowIcon(props: SVGProps<SVGSVGElement>) {
+	return (
+		<svg
+			width="800px"
+			height="800px"
+			viewBox="0 0 24 24"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+			{...props}
+		>
+			<path
+				d="M6 12H18M18 12L13 7M18 12L13 17"
+				stroke="currentColor"
+				strokeWidth="2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
+		</svg>
+	);
+}
